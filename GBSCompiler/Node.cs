2964780fcebda35759ff1cb18afdce9990ce636f
themilnera <amilner32@gmail.com>
2026-07-01 12:06:48 -0600

    Completed the inital version of the scanner and busy puzzling out how to implement the parser.

---
+++ GBSCompiler/Node.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBSCompiler
+{
+	internal class Node
+	{
+		public string Kind {  get; set; }
+		public Node(string kind){
+			this.Kind = kind;	
+		}
+	}
+	internal class Program : Node
+	{
+		public Node[] Body { get; set; }
+		public Program(string kind, Node[] body) : base(kind)
+		{
+			Body = body;
+		}
+	}
+
+	internal class Assignment : Node
+	{
+		public Node Value { get; set; }
+		public Assignment(string kind, Node value): base(kind){
+			Value = value;
+		}
+	}
+
+
+}
--- GBSCompiler/Parser.cs
+++ GBSCompiler/Parser.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace GBSCompiler
 {
+	
     internal class Parser
     {
+		
     }
 }
