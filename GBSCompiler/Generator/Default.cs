8ea3330402a515ba131fd180f2d5a89b83326f45
themilnera <amilner32@gmail.com>
2026-07-07 12:40:54 -0600

    Improved inline asm insert parsing

--- GBSCompiler/Generator/Default.cs
+++ GBSCompiler/Generator/Default.cs
@@ -11,58 +11,63 @@ namespace GBSCompiler
 		public static List<string> GetDefaults(){
 			emit("INCLUDE \"hardware.inc\"");
 			emit("SECTION \"Header\", ROM0[$100]");
+			emit("nop");
+			emit("jp TurnOffLCD");
 			emit("ds $150 - @, 0");
+			emit("");
+			
 			emit("SECTION \"VBlank Interrupt\", ROM0[$40]");	
 			emit("VBlankInterrupt:");	
 			emit("push af\npush bc\npush de\npush hl");	
-			emit("jp VBlankHandler");	
-			emit("SECTION \"Variables\", WRAM0");
-			emit("wCurKeys: db");
-			emit("wNewKeys: db");
-			emit("OAMBuffer: ds 160");
+			emit("jp VBlankHandler");
 			emit("");
-			emit("SECTION \"Code\", ROM0[$150]");	
-			emit("ld a, [rLCDC] ;turn off lcd");	
-			emit("bit 7, a");
-			emit("jr z, .off");
-			emit(".wait:");
-			emit("ld a, [rLY]");
-			emit("cp 144");
-			emit("jr c, .wait");
-			emit(".off");
-			emit("xor a");
-			emit("ld [rLCDC], a");
-			emit("");
+
+			emit("SECTION \"Code\", ROM0[$150]");
+			emit("SCODI:");
+			emit("END_SCODI:");
+			emit("CopyRunDMA:");
 			emit("ld hl, RunDMA ;copy RunDMA to HRAM");
 			emit("ld de, $FF80");
 			emit("ld bc, EndDMA - RunDMA");
 			emit("call MemCopy");
+			emit("");
+
+			emit("InitVars:");
 			emit("ld a, 0");
 			emit("ld [wCurKeys], a");
 			emit("ld [wNewKeys], a");
 			emit("ld [GameState], a");
 			emit("jp Start");
 			emit("");
-			emit("MemCopy:");
-			emit("ld a, [de]");
-			emit("ld [hli], a");
-			emit("inc de");
-			emit("dec bc");
-			emit("ld a, b");
-			emit("or a, c");
-			emit("jp nz, MemCopy");
-			emit("ret");
+
+			emit("VBlankHandler:");
+			emit("call UpdateKeys");
+			emit("call $FF80 ;RunDMA in HRAM");
+			emit("VBHI:");
+			emit("END_VBHI:");
+			emit("pop af");
+			emit("pop bc");
+			emit("pop de");
+			emit("pop hl");
+			emit("reti");
 			emit("");
-			emit("RunDMA:");
-			emit("ld a, HIGH(OAMBuffer)");
-			emit("ldh [rDMA], a");
-			emit("ld a, 40");
-			emit(".wait");
-			emit("dec a");
-			emit("jr nz, .wait");
-			emit("ret");
-			emit("EndDMA:");
+
+			emit("Start:");
+			emit("INITI:");
+			emit("END_INITI:");
+			emit("call TurnOnLCDPalette0");
+			emit("jp Main");
 			emit("");
+
+			emit("Main:");
+			emit("halt");
+			emit("jp Main");
+			emit("");
+			
+			emit("PMI:");
+			emit("END_PMI:");
+			emit("");
+
 			emit("UpdateKeys:");
 			emit("ld a, JOYP_GET_BUTTONS");
 			emit("call .onenibble");
@@ -87,38 +92,66 @@ namespace GBSCompiler
 			emit("ldh a, [rJOYP]");
 			emit("ldh a, [rJOYP]");
 			emit("ldh a, [rJOYP]");
-			emit("or a, $FO");
+			emit("or a, $F0");
 			emit(".knownret");
 			emit("ret");
 			emit("");
-			emit("VBlankHandler:");
-			emit("call UpdateKeys");
-			emit("call RunDMA");
-			emit("VBHI:");
-			emit("END_VBHI:");
-			emit("pop af");
-			emit("pop bc");
-			emit("pop de");
-			emit("pop hl");
-			emit("reti");
-			emit("Start:");
+
+			emit("MemCopy:");
+			emit("ld a, [de]");
+			emit("ld [hli], a");
+			emit("inc de");
+			emit("dec bc");
+			emit("ld a, b");
+			emit("or a, c");
+			emit("jp nz, MemCopy");
+			emit("ret");
+			emit("");
+
+			emit("RunDMA:");
+			emit("ld a, HIGH(OAMBuffer)");
+			emit("ldh [rDMA], a");
+			emit("ld a, 40");
+			emit(".wait");
+			emit("dec a");
+			emit("jr nz, .wait");
+			emit("ret");
+			emit("EndDMA:");
+			emit("");
+
+			emit("TurnOffLCD:");
+			emit("ld a, [rLCDC]");
+			emit("bit 7, a");
+			emit("jr z, .off");
+			emit(".wait:");
+			emit("ld a, [rLY]");
+			emit("cp 144");
+			emit("jr c, .wait");
+			emit(".off");
+			emit("xor a");
+			emit("ld [rLCDC], a");
+			emit("ret");
+			emit("");
+
+			emit("TurnOnLCDPalette0:");
 			emit(";default palette, turn on screen");
 			emit("ld a, LCDC_ON | LCDC_BG_ON | LCDC_OBJ_ON");
 			emit("ld [rLCDC], a");
 			emit("ld a, %11100100");
 			emit("ld [rOBP0], a");
 			emit("ld [rBGP], a");
+			emit("ret");
 			emit("");
-			emit("INITI:");
-			emit("END_INITI:");
-			emit("");
-			emit("Main:");
-			emit("halt");
-			emit("jp Main");
-			emit("PMI:");
-			emit("END_PMI:");
-			
 
+			emit("SECTION \"Variables\", WRAM0");
+			emit("wCurKeys: db");
+			emit("wNewKeys: db");
+			emit("GameState: db");
+			emit("OAMBuffer: ds 160");
+			emit("");
+
+			emit("BI:");
+			emit("ENDBI:");
 			return lines;
 		}
 	}
--- GBSCompiler/Generator/Generator.cs
+++ GBSCompiler/Generator/Generator.cs
@@ -6,37 +6,21 @@ namespace GBSCompiler
 	{
 		List<string> lines = new List<string>();
 		string inlineAsm = "";
+		private string[] inserts = ["PMI","VBHI","INITI","BI","SCODI"];
 		private void emit(string line) {
 			lines.Add(line + "\n");
 		}
 
 		private void parseInline(){
-			if (inlineAsm.Contains("PMI:") && inlineAsm.Contains("END_PMI:"))
-			{
-				int s_i = inlineAsm.IndexOf("PMI:");
-				int s_end_i = inlineAsm.IndexOf("END_PMI:");
-				int d_i = lines.IndexOf("PMI:\n");
-				lines.RemoveAt(d_i);
-				string block = inlineAsm.Substring(s_i, s_end_i - s_i);
-				lines.Insert(d_i, block);
-			}
-			if (inlineAsm.Contains("VBHI:") && inlineAsm.Contains("END_VBHI:"))
-			{
-				int s_i = inlineAsm.IndexOf("VBHI:");
-				int s_end_i = inlineAsm.IndexOf("END_VBHI:");
-				int d_i = lines.IndexOf("VBHI:\n");
-				lines.RemoveAt(d_i);
-				string block = inlineAsm.Substring(s_i, s_end_i - s_i);
-				lines.Insert(d_i, block);
-			}
-			if (inlineAsm.Contains("INITI:") && inlineAsm.Contains("END_INITI:"))
-			{
-				int s_i = inlineAsm.IndexOf("INITI:");
-				int s_end_i = inlineAsm.IndexOf("END_INITI:");
-				int d_i = lines.IndexOf("INITI:\n");
-				lines.RemoveAt(d_i);
-				string block = inlineAsm.Substring(s_i, s_end_i - s_i);
-				lines.Insert(d_i, block);
+			foreach(string insert in inserts){
+				if(inlineAsm.Contains(insert+":") && inlineAsm.Contains("END_"+insert+":")){
+					int s_i = inlineAsm.IndexOf(insert + ":");
+					int s_end_i = inlineAsm.IndexOf("END_" + insert + ":");
+					int d_i = lines.IndexOf(insert + ":\n");
+					lines.RemoveAt(d_i);
+					string block = inlineAsm.Substring(s_i, s_end_i - s_i);
+					lines.Insert(d_i, block);
+				}
 			}
 		}
 		public string GenerateCode(Node node){
